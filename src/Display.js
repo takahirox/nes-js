8f2d36d6328932e06b7b45ffe75e362da382c710
takahiro <hogehoge@gachapin.jp>
2014-08-13 02:20:13 -0700

    temporal commit.

--- src/Display.js
+++ src/Display.js
@@ -3,8 +3,9 @@ function Display(canvas, nes) {
   this.nes = nes;
   this.ctx = canvas.getContext('2d');
 
-  this.cvs.width = Display._WIDTH * Display._PIXEL_SIZE;
-  this.cvs.height = Display._HEIGHT * Display._PIXEL_SIZE;
+  this.width = this.cvs.width = Display._WIDTH * Display._PIXEL_SIZE;
+  this.height = this.cvs.height = Display._HEIGHT * Display._PIXEL_SIZE;
+
 };
 
 Display._WIDTH = 256;
@@ -12,7 +13,8 @@ Display._HEIGHT = 240;
 Display._PIXEL_SIZE = 2;
 
 Display.prototype.update = function() {
-  this.ctx.clearRect(0, 0, Display._WIDTH, Display._HEIGHT);
+  this.ctx.clearRect(0, 0, this.width, this.height);
+/*
   for(var i = 0; i < Display._WIDTH; i++) {
     for(var j = 0; j < Display._HEIGHT; j++) {
       var color = this.nes.ppu.getPixelRGB(i, j);
@@ -23,6 +25,25 @@ Display.prototype.update = function() {
                         Display._PIXEL_SIZE);
     }
   }
+*/
+  var data = this.ctx.createImageData(this.width, this.height);
+  var pos = 0;
+  for(var i = 0; i < Display._HEIGHT; i++) {
+    for(var j = 0; j < Display._WIDTH; j++) {
+      var color = this.nes.ppu.getPixelRGB(j, i);
+      for(var k = 0; k < Math.pow(Display._PIXEL_SIZE, 2); k++) {
+        var index = (i * this.width * Display._PIXEL_SIZE +
+                     j * Display._PIXEL_SIZE +
+                     k % Display._PIXEL_SIZE +
+                     parseInt(k / Display._PIXEL_SIZE) * this.width) * 4;
+        data.data[index+0] = color[0];
+        data.data[index+1] = color[1];
+        data.data[index+2] = color[2];
+        data.data[index+3] = 255;
+      }
+    }
+  } 
+  this.ctx.putImageData(data, 0, 0);
 };
 
 
