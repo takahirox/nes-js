ff9bb282dc815ef197e9debc8bd8e7d8b02f8fbc
Takahiro <hogehoge@gachapin.jp>
2017-08-27 13:05:54 +0900

    Clean up Display.js

--- src/Display.js
+++ src/Display.js
@@ -1,24 +1,25 @@
+/**
+ *
+ */
 function Display(canvas) {
-  this.cvs = canvas;
   this.ctx = canvas.getContext('2d');
 
-  this.width = this.cvs.width = this._WIDTH;
-  this.height = this.cvs.height = this._HEIGHT;
+  this.width = canvas.width = 256;
+  this.height = canvas.height = 240;
 
   this.data = this.ctx.createImageData(this.width, this.height);
   this.uint32 = new Uint32Array(this.data.data.buffer);
-};
+}
 
-Display.prototype._WIDTH = 256;
-Display.prototype._HEIGHT = 240;
+Object.assign(Display.prototype, {
+  isDisplay: true,
 
+  renderPixel: function(x, y, c) {
+    var index = y * this.width + x;
+    this.uint32[index] = c;
+  },
 
-Display.prototype.renderPixel = function(x, y, c) {
-  var index = y * this.width + x;
-  this.uint32[index] = c;
-};
-
-
-Display.prototype.updateScreen = function() {
-  this.ctx.putImageData(this.data, 0, 0);
-};
+  updateScreen: function() {
+    this.ctx.putImageData(this.data, 0, 0);
+  }
+});
