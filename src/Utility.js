eaebd0b230632617a34967bbbc7b0d55a308ca37
Takahiro <hogehoge@gachapin.jp>
2017-09-20 22:43:39 +0900

    Update Utility.convertDecToHex

--- src/Utility.js
+++ src/Utility.js
@@ -2,17 +2,29 @@ function Utility() {
 
 }
 
-Utility.convertDecToHexString = function(num, figure, noPrefix) {
+/**
+ *
+ */
+Utility.convertDecToHexString = function(num, width, noPrefix) {
   var str = num.toString(16);
-  var base = '';
-  var prefix = (noPrefix == null) ? '0x' : '';
 
-  if(figure != null) {
-    for(var i = 0; i < figure; i++)
-      base += '0';
-    return prefix + (base + str).substr(-1*figure);
-  }
-  return prefix + str;
+  var prefix = '';
+
+  if(num < 0)
+    prefix += '-';
+
+  if(noPrefix !== true)
+    prefix += '0x';
+
+  if(width === undefined)
+    return prefix + str;
+
+  var base = '';
+
+  for(var i = 0; i < width; i++)
+    base += '0';
+
+  return prefix + (base + str).substr(-1 * width);
 };
 
 
